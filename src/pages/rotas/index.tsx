072001028c1b02e659b14ff8f52c76d30d65cfe0
Marcos Sabatini <marcos.sabatini@ionsistemas.com.br>
2023-02-08 17:51:45 -0300

    navegação por push

--- src/pages/rotas/index.tsx
+++ src/pages/rotas/index.tsx
@@ -1,6 +1,23 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Rotas() {
+	const router = useRouter()
+
+	function navegacaoSimplesPorNome(url: string) {
+		router.push(url)
+	}
+
+	function navegacaoParams() {
+		router.push({
+			pathname: '/rotas/params',
+			query: {
+				nome: 'Marcos Sabatini',
+				email: 'ms@email.com'
+			}
+		})
+	}
+
 	return (
 		<>
 			<h1 style={{ marginBottom: 10 }}>Rotas Index</h1>
@@ -16,6 +33,25 @@ export default function Rotas() {
 					<li>Buscar por Ana</li>
 				</Link>
 			</ul>
+
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'flex-start',
+					paddingTop: 10
+				}}
+			>
+				<button onClick={() => router.push('/rotas/443322/buscar')}>
+					Buscar
+				</button>
+				<button onClick={() => navegacaoSimplesPorNome('/rotas/123654/carlos')}>
+					Buscar por nome do Carlos
+				</button>
+				<button onClick={() => navegacaoParams()}>
+					Buscar por nome do Marcos
+				</button>
+			</div>
 		</>
 	)
 }
